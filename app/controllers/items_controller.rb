9f6497427adad4c0513a0c0c7175e2f2bcdd780f
gaba4545 <69068211+gaba4545@users.noreply.github.com>
2020-09-16 01:20:45 +0900

    何かの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,32 @@
 class ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+     if @item.save
+      redirect_to root_path
+     else
+      render :new
+       
+     end
+    end
+
+  #   @item.save
+  #   redirect_to root_path
+  # end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :explanation, :category_id, :status_id, :shipping_charges_id, :shipping_origin_area_id, :days_until_shipping_id, :image).merge(user_id: current_user.id)
+  end
+
 end
---
+++ app/models/item.rb
@@ -0,0 +1,25 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :shipping_charges
+  belongs_to_active_hash :shipping_origin_area
+  belongs_to_active_hash :days_until_shipping
+
+  belongs_to :user
+
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :price,                          numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/}
+    validates :explanation
+    validates :category_id,                    numericality: { other_than: 0 }
+    validates :status_id,                      numericality: { other_than: 0 }
+    validates :shipping_charges_id,            numericality: { other_than: 0 }
+    validates :shipping_origin_area_id,        numericality: { other_than: 0 }
+    validates :days_until_shipping_id,         numericality: { other_than: 0 }
+    validates :image
+  end
+
+end
