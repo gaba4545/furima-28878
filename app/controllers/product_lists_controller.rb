0a21118c67e736a866db8199caeb0d767e94c25a
gaba4545 <69068211+gaba4545@users.noreply.github.com>
2020-09-18 13:25:57 +0900

    商品一覧表示機能

---
+++ app/controllers/product_lists_controller.rb
@@ -0,0 +1,21 @@
+class ProductListsController < ApplicationController
+
+  def index
+    @product_lists = Product_lists.all
+  end
+
+  def create
+    @product_list = Item.new(product_list_params)
+     if @product_list.valid?
+      @product_list.save
+      redirect_to root_path
+     else
+      render :new
+     end
+  end
+
+  def product_list_params
+    params.require(:product_list).permit( :name, :price, :explanation, :category_id, :status_id, :shipping_charges_id, :shipping_origin_area_id, :days_until_shipping_id, :image).merge(user_id: current_user.id)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'product/list'
   devise_for :users
   root 'items#index'
   resources :items
   resources :users, only: [:edit, :update]
+  resources :product_lists, only: [:index]
 end
