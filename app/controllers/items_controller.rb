3d51c2033bc2b34fe6c2a59829ef242eed7bbbbf
gaba4545 <69068211+gaba4545@users.noreply.github.com>
2020-10-01 21:55:56 +0900

    商品情報編集機能3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  before_action :
+  before_action :set_item, only:[:show,:edit,:update]
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -24,24 +24,24 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
-  def update 
-    @item = Item.find(params[:id])
-    @item.update(item_params)
-    if @item.valid?
-      @item.save
+  def update
+    if @item.update(item_params)
       redirect_to root_path
     else
       render :edit
     end
   end
-
+  
   private
 
   def item_params
     params.require(:item).permit( :name, :price, :explanation, :category_id, :status_id, :shipping_charges_id, :shipping_origin_area_id, :days_until_shipping_id, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
