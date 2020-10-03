e475caa05bf09d64eb7d4657e663a1ef8cf9fe46
gaba4545 <69068211+gaba4545@users.noreply.github.com>
2020-10-03 15:32:49 +0900

    商品削除機能2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  before_action :set_item, only:[:show,:edit,:update]
+  before_action :set_item, only:[:show,:edit,:update,:destroy]
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -20,10 +20,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
      if @item.user_id == current_user.id
       @item.destroy
       redirect_to root_path
+     else
+       render :edit
      end
    end
 
