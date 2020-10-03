66505aefe148c3649cd12a3475f2d4fcce060787
gaba4545 <69068211+gaba4545@users.noreply.github.com>
2020-10-03 15:49:20 +0900

    商品削除機能3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,8 +20,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-     if @item.user_id == current_user.id
-      @item.destroy
+     if @item.user_id == current_user.id && @item.destroy
       redirect_to root_path
      else
        render :edit
