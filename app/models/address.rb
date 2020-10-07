023842232f434ea5046fa2d1f6e48318f1ff9992
gaba4545 <69068211+gaba4545@users.noreply.github.com>
2020-10-07 14:20:17 +0900

    モデルのバリデイト設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,15 +1,12 @@
 class Address < ApplicationRecord
-  # validates :price, presence: true
-  # 郵便番号が必須であること
-  # validates :postal_code
-  # 都道府県が必須であること
-  # validates :shipping_origin_area
-  # 市区町村が必須であること
-  # validates :municipality
-  # 番地が必須であること
-  # validates :building_number
-  # 電話番号が必須であること
-  # validates :address
-  # 郵便番号にはハイフンが必要であること
-  
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+
+            # 郵便番号、都道府県、市区町村、番地、電話番号が必須であること
+            validates :postal_code,:shipping_origin_area,:municipality,:building_number,:phone_number,presence: true
+            # 郵便番号にはハイフンが必要であること
+            validates :postal_code, format: {with:/\A\d{3}[-]\d{4}\z/}
+            # 電話番号にはハイフンは不要で、11桁以内であること
+            validates :phone_number, format: {with:/\A\d{11}\z/}
 end
